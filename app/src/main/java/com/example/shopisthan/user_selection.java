32dfca737051767a1b24f40208866f9c71e2fb46
sohailqureshiii <sohail.qureshi@adypu.edu.in>
2021-02-20 20:10:12 +0530

    mene bhi kardiya

--- app/src/main/java/com/example/shopisthan/user_selection.java
+++ app/src/main/java/com/example/shopisthan/user_selection.java
@@ -1,6 +1,7 @@
 package com.example.shopisthan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,14 +10,14 @@ import android.widget.ImageView;
 
 public class user_selection extends AppCompatActivity {
 
-    ImageView shopcreate;
+    CardView shopcreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_selection);
 
-        shopcreate = findViewById(R.id.usersform);
+        shopcreate = findViewById(R.id.sellersform);
         shopcreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
